15fb35cbaed9406ffd1c81d2f78945772dc3c8fb
Giao <48171973+giaodev@users.noreply.github.com>
2021-06-18 15:03:00 +0700

    Add files via upload

---
+++ class.installment.php
@@ -0,0 +1,18 @@
+<?php
+class Installment {
+
+	private static $initiated = false;
+	public static function init() {
+		if ( ! self::$initiated ) {
+			self::init_hooks();
+		}
+	}
+
+	/**
+	 * Initializes WordPress hooks
+	 */
+	private static function init_hooks() {
+		self::$initiated = true;
+	}
+	
+}
